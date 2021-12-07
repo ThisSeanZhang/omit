e4178e6e0d5bec4c6bdca590dfd9f5fed1ecc55d
ThisSeanZhang <thisseanzhang@gmail.com>
2021-12-07 17:25:07 +0800

    try to change conpty

--- src-tauri/src/ssh_cmd.rs
+++ src-tauri/src/ssh_cmd.rs
@@ -8,6 +8,8 @@ use crossbeam::channel::{Receiver, Sender, TryRecvError, unbounded};
 use uuid::Uuid;
 use serde::{Serialize, Deserialize};
 
+use io::prelude::{conpty, Process};
+
 use tauri::{AppHandle, Window, WindowUrl, command, State};
 
 pub struct SSHState(pub Arc<Mutex<HashMap<String, SSHSessionManage>>>);
@@ -31,11 +33,86 @@ enum ChannelAction {
     }
 }
 
-fn read_channel(channel: &mut Channel, action_get: Receiver<ChannelAction>, window: Window) {
+// fn read_channel(channel: &mut Channel, action_get: Receiver<ChannelAction>, window: Window) {
+//     println!("stare read channel");
+//     let mut buf = [0; 4096];
+//     while !channel.eof() {
+//         match channel.read(&mut buf) {
+//             Ok(size) => {
+//                 let s = String::from_utf8(buf[0..size].to_vec()).unwrap();
+//                 println!("size: {}, s len: {} {}", size, s.len(),s);
+//                 window.emit("ssh-data-from-backend", SSHMessage { data: buf[0..size].to_vec() }).unwrap();
+//             }
+//             Err(e) => {
+//                 if e.kind() != std::io::ErrorKind::WouldBlock {
+//                     println!("err {}", e);
+//                     break;
+//                 }
+//             }
+//         }
+//         if !action_get.is_empty() {
+//             match action_get.try_recv() {
+//                 Ok(action) => {
+//                     match action {
+//                         ChannelAction::Message(line) => {
+//                             channel.write(line.as_bytes()).unwrap();
+//                             channel.flush().unwrap();
+//                         },
+//                         ChannelAction::SizeChange{ width, height, width_px, height_px } => {
+//                             channel.request_pty_size(width, height, width_px, height_px);
+//                         },
+//                         _ => println!("")
+//                     }
+//                 }
+//                 Err(TryRecvError::Empty) => {
+//                     println!("{}", "empty");
+//                 }
+//                 Err(TryRecvError::Disconnected) => {
+//                     println!("{}", "disconnected");
+//                 }
+//             }
+//         }
+//         // if !receiver.is_empty() {
+//         //     match receiver.try_recv() {
+//         //         Ok(line) => {
+                    
+//         //             // let cmd_string = if line == "\n" {
+//         //             //     "\n".to_string()
+//         //             // } else {
+//         //             //     line + "\n"
+//         //             // };
+//         //             // println!("{:?}", line.bytes());
+//         //             // let cmd_string = line + "\n";
+//         //             channel.write(line.as_bytes()).unwrap();
+//         //             channel.flush().unwrap();
+//         //             // channel.write_fmt(format_args!("{}\n", line)).unwrap();
+//         //         }
+  
+//         //         Err(TryRecvError::Empty) => {
+//         //             println!("{}", "empty");
+//         //         }
+  
+//         //         Err(TryRecvError::Disconnected) => {
+//         //             println!("{}", "disconnected");
+//         //         }
+//         //         // Err(e) => {
+//         //         //     println!("{:?}", e);
+//         //         // }
+//         //         // _=> ()
+//         //     }
+//         // }
+//         std::thread::sleep(std::time::Duration::from_millis(5));
+//     }
+//     window.emit("ssh-data-from-backend", SSHMessage { data: "lose connection".into() }).unwrap();
+// }
+
+fn read_pty_output(process: &mut Process, action_get: Receiver<ChannelAction>, window: Window) {
     println!("stare read channel");
     let mut buf = [0; 4096];
-    while !channel.eof() {
-        match channel.read(&mut buf) {
+    let output = process.output().unwrap();
+    let mut input = process.input().unwrap();
+    while process.is_alive() {
+        match output.read(&mut buf) {
             Ok(size) => {
                 let s = String::from_utf8(buf[0..size].to_vec()).unwrap();
                 println!("size: {}, s len: {} {}", size, s.len(),s);
@@ -53,11 +130,11 @@ fn read_channel(channel: &mut Channel, action_get: Receiver<ChannelAction>, wind
                 Ok(action) => {
                     match action {
                         ChannelAction::Message(line) => {
-                            channel.write(line.as_bytes()).unwrap();
-                            channel.flush().unwrap();
+                            input.write(line.as_bytes()).unwrap();
+                            input.flush().unwrap();
                         },
                         ChannelAction::SizeChange{ width, height, width_px, height_px } => {
-                            channel.request_pty_size(width, height, width_px, height_px);
+                            process.resize(width, height);
                         },
                         _ => println!("")
                     }
@@ -137,21 +214,11 @@ pub fn create_ssh(window: Window, ssh_state: State<SSHState>, SSHInfo { ip, port
             return;
         }
     }
-    let tcp = TcpStream::connect(format!("{}:{}", ip, port)).unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_password(username, passwd).unwrap();
-    let mut channel = sess.channel_session().unwrap();
-    channel.request_pty(
-        "xterm",
-         None,
-         Some((129, 33, 0, 0)),
-        ).unwrap();
-    channel.shell().unwrap();
     
-    sess.set_blocking(false);
-    // let (trx, rev):(Sender<String>, Receiver<String>) = unbounded();
+    let ssh_proc = conpty::spawn(format!("ssh {}@{} -p {}", username, ip, port)).unwrap();
+    ssh_proc.resize(129, 33);
+    
+    
     let (action_send, action_get) = unbounded();
     let line_send = action_send.clone();
     let front_listen_id = window.listen("ssh-data-from-frontend", move |event| {
@@ -166,12 +233,12 @@ pub fn create_ssh(window: Window, ssh_state: State<SSHState>, SSHInfo { ip, port
         }
     });
     thread::spawn(move || {
-        read_channel(&mut channel, action_get, window);
+        read_pty_output(ssh_proc, action_get, window);
     });
     // let my_uuid = Uuid::new_v4();
-    ssh_state.0.lock().unwrap().insert(window_label, SSHSessionManage { 
-        sess,
-    });
+    // ssh_state.0.lock().unwrap().insert(window_label, SSHSessionManage { 
+    //     sess,
+    // });
 }
 
 /*
